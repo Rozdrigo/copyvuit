94c286f69b8eadab187d9f05f17b0d8fd99d3d33
Rozdrigo <rodrigo.coelho@linkedby.com.br>
2023-09-20 13:18:25 -0300

    [001] - Alter ConnectionString

--- vuit.api/src/database/dbcontext.js
+++ vuit.api/src/database/dbcontext.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function main() {
-  await mongoose.connect("mongodb://0.0.0.0:27017/vuit2")
+  await mongoose.connect("mongodb://0.0.0.0:27017/vuit")
 }
 
 main();
